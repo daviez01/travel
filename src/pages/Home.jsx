1162ef56ee9c8510838a88b2d2bfab058539b8ff
Davies Ilori <daviesilori@gmail.com>
2023-08-14 17:05:50 +0100

    almost done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 
 import Hero from "../components/Hero/Hero"
+import Services from "../components/Services/Services"
+import Romantic from "../components/Romantic/Romantic"
+import Trip from "../components/Trip/Trip"
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Services />
+      <Romantic />
+      <Trip />
     </div>
   )
 }
